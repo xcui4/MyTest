7d20cb21ce21d257d9d05875be31a88748b8aadc
xcui4 <xuguangcui@gmail.com>
2012-11-23 09:58:15 +0800

    Instantiate
    // Instantiate delegate with named method:
    // Invoke delegate ma:
    // Instantiate delegate with anonymous method:
    // Instantiate delegate with lambda expression

--- DelegateTest/DelegateTest/Program.cs
+++ DelegateTest/DelegateTest/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace DelegateTest
 {
@@ -9,6 +6,38 @@ namespace DelegateTest
     {
         static void Main(string[] args)
         {
+            var test = new Test();
+            var a = Console.ReadLine();
+            var b = Console.ReadLine();
+
+            test.print(a,b);
+            Console.ReadKey();
         }
     }
+        
+    public delegate string StringAction(string a, string b);
+
+    public delegate int IntegerAction(int a, int b);
+
+    public class Test
+    {
+        public static string CombineTwoStrings(string a, string b)
+        {
+            return a+b;
+        }
+
+        private readonly StringAction m_CombineTwoStrings = CombineTwoStrings;
+
+        private StringAction SA = (a, b) => a + b;
+
+        private IntegerAction IA = (a, b) => a + b;
+
+        public void print(string a, string b)
+        {
+            Console.WriteLine("CombineString: {0}",m_CombineTwoStrings(a,b));
+            Console.WriteLine("SA: {0}", SA(a, b));
+            Console.WriteLine("IA: {0}", IA(Convert.ToInt32(a), Convert.ToInt32(b)));
+        }
+    }
+
 }
